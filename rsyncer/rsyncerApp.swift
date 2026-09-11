a06da08e107f9802b356ba121aa6ad097c291b95
ludehon <luciendehon@gmail.com>
2026-09-11 11:17:03 +0100

    Initial Commit

---
+++ rsyncer/rsyncerApp.swift
@@ -0,0 +1,17 @@
+//
+//  rsyncerApp.swift
+//  rsyncer
+//
+//  Created by lucien on 11/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct rsyncerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
